2825e9d2f9a351e14f1643cb95bb8fe606d88220
Nikita Martyanov <117271258+NikitaTurbo@users.noreply.github.com>
2025-03-30 20:27:01 +0300

    fix

--- execute.py
+++ execute.py
@@ -1,3 +1,16 @@
+import sys
 from interpreter import DogLangInterpreter as dg
 
-dg.execute("...") # ... can be code for example from a file
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <file.dog>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            code = file.read()
+        dg().execute(code)
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        sys.exit(1)
